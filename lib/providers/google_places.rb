c9ad850d2ab2ce398e53d1dad64aadeefd38eb44
flavio <flavio.mello@taqtile.com>
2015-03-21 20:36:27 -0300

    Creating method to gather detail of restaurant

--- lib/providers/google_places.rb
+++ lib/providers/google_places.rb
@@ -3,6 +3,7 @@ module GooglePlaces
   require 'rest_client'
   require 'json'
 
+  GOOGLE_API_KEY = "INSERT GOOGLE API KEY"
   GOOGLE_API_HOST = "https://maps.googleapis.com/maps/api/place/"
   GOOGLE_API_PATH_SEARCH = "nearbysearch/json"
   GOOGLE_API_PATH_DETAIL = "details/json"
@@ -23,14 +24,25 @@ module GooglePlaces
     params["types"] = "food"
     params["rankby"] = "prominence" #The search is for restaurants in a large area. Better to rank by prominence than radius
     params["name"] = restaurantName
-    params["key"] = "GOOGLE_API_KEY"
+    params["key"] = GOOGLE_API_KEY
 
     response = RestClient.get url, {:params => params}
     parsed = JSON.parse(response)
 
-    return parsed
+    return parsed["results"]
   end
 
+  # https://developers.google.com/places/webservice/search
+  def self.search_for_restaurant_detail (restaurantId)
+    url = "#{GOOGLE_API_HOST}#{GOOGLE_API_PATH_DETAIL}"
+    params = Hash.new
+    params["placeid"] = restaurantId
+    params["key"] = GOOGLE_API_KEY
 
+    response = RestClient.get url, {:params => params}
+    parsed = JSON.parse(response)
+
+    return parsed["result"]
+  end
 
 end
